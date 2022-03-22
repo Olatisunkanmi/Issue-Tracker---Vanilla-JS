b21c6f4d1e4022e2527290074a847c832785bc5d
Olatisunkanmi <igbasanolasunkanmi080@gmail.com>
2022-03-22 21:35:27 +0100

    Pushing P
    Keep pushing

--- js/script.js
+++ js/script.js
@@ -1,40 +1,97 @@
 
+
             // this functiion  handles the informaion retrieved from the DOm and Dom altering
             const UiController = (() => {
 
-                const getDom =  {
-                    inputType: '.form',
+                const getAllDom =  {
+                    inputType: '.add--type',
                     inputDescription:'.description',
                     inputAssigned: '.assigned',
-                    issuesInputBtn: '.create--issues'                    
+                    issuesInputBtn: '.create--issues',
+                    lowDiv: '.Low-div',
+                    midDiv: '.Mid-div',
+                    highDiv: '.High-div'
                 }
             
                 
-                return{
+          return{
 
-                    domInput: () => {
-                        return {
-                            type:document.querySelector(getDom.inputType).value,
-                            description: document.querySelector(getDom.inputDescription).value, 
-                            assigned: document.querySelector(getDom.inputAssigned).value
-                        }
-                    },
+                            domInput: () => {
+                                return {
 
-                    allDom :() => getDom
-                };
-            })()
+                                    description: document.querySelector(getAllDom.inputDescription).value, 
+                                    type:document.querySelector(getAllDom.inputType).value,
+                                    assigned: document.querySelector(getAllDom.inputAssigned).value
+                                }
+                            },
 
+                            allDom :() => {
+                                return getAllDom;
+                            },
 
+                                        /**
+                                         * this function accepts piority, and the array of the newly create Item to input into the doM.
+                                         * this is the function that access the DOM
+                                         * 
+                                         */
+                            addListItem: (type, newItem) => {
+                            let html, element, newHtml;
+                                        if (type === 'High') {                                
+                                        element = getAllDom.highDiv;
+                                        html = '<div id="High-%id"><h6>Issue ID: </h6><h5 class="status"></h5><p class="priority"><i class="fas fa-clock"></i>%Priority</p> '+ 
+                                        '<h3 class="description--value"> %description</h3><p><i class="fas fa-address-card assigned--value"></i> %Assigned </p> '+
+                                        '<a href="#"  class="btn btn-warning">Close</a><a href="#"  class="btn btn-danger">Delete</a></div>'
+                               
+                                    }   
+                                    
+                                    else if(type === 'Medium'){
+                                   
+                                    element = getAllDom.midDiv;
+                                    html = '<div id="Medium-%id"><h6>Issue ID: </h6><h5 class="status"></h5><p class="priority"><i class="fas fa-clock"></i>%Priority</p> '+ 
+                                    '<h3 class="description--value"> %description</h3><p><i class="fas fa-address-card assigned--value"></i> %Assigned </p> '+
+                                    '<a href="#"  class="btn btn-warning">Close</a><a href="#"  class="btn btn-danger">Delete</a></div>'
+                           
+                                }  
+                                
+                                else if(type === 'Low'){
+                                    element = getAllDom.highDiv;
+                                    html = '<div id="Low-%id"><h6>Issue ID: </h6><h5 class="status"></h5><p class="priority"><i class="fas fa-clock"></i>%Priority</p> '+ 
+                                    '<h3 class="description--value"> %description</h3><p><i class="fas fa-address-card assigned--value"></i> %Assigned </p> '+
+                                    '<a href="#"  class="btn btn-warning">Close</a><a href="#"  class="btn btn-danger">Delete</a></div>'
+                               
+                                }
+
+                                // newHtml = html.replace('%id', newItem.id);
+                              
+                                newHtml = html.replace('%description', newItem.des);
+                                newHtml = newHtml.replace('%Assigned', newItem.assigned);
+                                
+
+                                document.querySelector(element).insertAdjacentHTML('beforebegin', newHtml);
+                                
+                          }
+                          
+              
+              }   /* closing braces for the return function*/  
+     })()
+                
+
+                   
+
+                        /**
+                         * A closure functon to accept data and arrange Items
+                         * created Items are set into arrays and given their ID for sorting
+                         */
             
             
             //              this function handles the data aninputValuet Issues
             const dataController = (() => {
 
             
-                class Items {
-                    constructor(description, piority, assigned) {
-                        this.description = description;
-                        this.piority = piority;
+                class allItemsFunc {
+                    constructor(id, des , assigned) {
+                        this.id = id;
+                        this.des = des;
                         this.assigned = assigned;
                     }
                 }
@@ -42,73 +99,113 @@
                 
                 const data = {
                     allItems: {
-                        high: [],
-                        mid: [],
-                        low: []
+                        High: [],
+                        Medium: [],
+                        Low: []
                         },
                 }
 
                 return {
                     // creating new items, item are grouped according to piority
-     createItems: (description, piority) => {
-
-                        if (piority === 'high') {
-                            Items = new Items();
-                                  }
-
-                             else  if (piority === 'medium') {
-                                 Items = new Expenses();
-                                      }
-
-                            else if (piority === 'low') {
-                                Items = new Expenses();
-                            }
-
-        // Pushing the newly created   income or expenses into our array
-            data.allItems[piority].push(newItem);
-
-            // returning the newly created element 
-            return newItem;
-                }
+                     createItems: (type, des, assigned) => {
+let newItem, ID,index, ids;
 
 
-    }
+        //----->>>>  ----->>>>>  ------->>>>>>>>    ------<<<<<<--------
+                            
+                        
+                                        if (data.allItems[type].length > 0) {
+                                            ID = data.allItems[type][data.allItems[type].length -1].id +1;
+                                            //   console.log(ID);
+                            
+                                        } else {
+                                            ID = 0;
+                                            //    console.log(ID);
+                                        }
+                                        
+                                
+                            if (type === 'High') {
+                                            newItem = new allItemsFunc(ID, des, assigned);
+                                                }
+
+                                else  if (type === 'Medium') {
+                                    newItem = new allItemsFunc(ID, des, assigned);
+                                }
+
+                                else if (type === 'Low') {
+                                    newItem = new allItemsFunc(ID, des, assigned);
+                                }
+                //  -------------->>>>>>>> ----->>>>>>>>>>>>>------------>>>>>>>><<<<<<<
+                        // Pushing the newly created   income or expenses into our array              
+                                data.allItems[type].push(newItem);
+                                // console.log(data.allItems[type]);
+
+                                // data.allItems.length
+                            ids = data.allItems[type].map(current => {
+                                return current.id;
+                                });
+
+                                // console.log( data.allItems[type]);
+                            // returning the newly created element 
+                            return newItem;
+                                }
+
+
+    }   
      })()
             
             
             
                             // This function comm. between the function and control all functions can not run independently.
- const trackerController = ((uiCtrl, dataCtrl) => {
-            
-     const init = () => {
-            let getDom = uiCtrl.allDom();
+                        const trackerController = ((uiCtrl, dataController) => {
 
-            document.querySelector(getDom.issuesInputBtn).addEventListener('click', ctrlAddItem);
+                            const init = () => {
 
-            document.addEventListener('keypress', function () {
-                if( event.keycode === 13 && event.which === 13 ){ctrlAddItem(); }
+                                // Getting the DOM strings 
+                            let getAllDom = uiCtrl.allDom();
+                                document.querySelector(getAllDom.issuesInputBtn).addEventListener("click", function() {
+                                ctrlAddItem()
 
-                    })
-        
-    }
+                                    })
+                        
+                            }
 
 
-             const ctrlAddItem = () => {
-                 const domInput = uiCtrl.domInput();
-                console.log(domInput);   
+                              const ctrlAddItem = () => {
+                
+                              const domInput = uiCtrl.domInput();
+                // {domInput.description, domInput.medium, domInput.assigned } : domInput
+                              if(domInput.description !== "" || domInput.assigned !== ""){  
+                              console.log(domInput.type);
 
-                if(domInput.description !== "" && domInput.assigned !== ""){
+                            /* Using the %priority of the tasks, description and assigned to 
+                                to create item and save each of them in an array
+                                
+                                so each item can be located easily for sorting.
+                                */
 
-                    console.log('Not MT');
-                  var newItem = dataController.createItems(domInput.description, domInput.piority, domInput.assigned );
-                  console.log(newItem);
-                } 
+
+                    const createItems = dataController.createItems(domInput.type, domInput.description, domInput.assigned)
+                                console.log(createItems);
+
+                        /*
+                        this function sends the piory to the other function to input into Dom
+                                                            <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+                                                            <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+                                                            <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<                 HI !! My name is Olasunkanmi                     <<<<<<<<<<
+                                                            <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+                                                            <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+                         */     
+                    const addEvents =  uiCtrl.addListItem(domInput.type, createItems);
+                                
+                       }
              }
 
                 
 
                 
-                return{
+        return{
                     initialize: () => {
                             console.log('Application is running');
                             init();
